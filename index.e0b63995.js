bedadd2f7e0e36a7bca51177332d86644b8f1d9c
olshum <olshum8@gmail.com>
2024-09-10 20:34:57 +0300

    make build

---
+++ index.e0b63995.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__list"),r=document.querySelector("#largeImg");e.addEventListener("click",function(e){var t=e.target.closest("a");r.src=t.href,e.preventDefault()});
+//# sourceMappingURL=index.e0b63995.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const ul = document.querySelector(".gallery__list");
+const largeImage = document.querySelector("#largeImg");
+// event were renamed due to eslit error
+ul.addEventListener("click", (evnt)=>{
+    const link = evnt.target.closest("a");
+    largeImage.src = link.href;
+    evnt.preventDefault();
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
